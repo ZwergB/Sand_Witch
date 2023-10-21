f6f9040da079749d6fddde1184800e332f5db69c
Kiirabu197 <konstantin.kreft@uol.de>
2023-10-21 18:42:18 +0200

    kleine aenderung

--- global.gd
+++ global.gd
@@ -17,8 +17,13 @@ class Ingredient:
 var inventory = [0,0,0,0,0,0,0]
 
 
-var ingredients = [Ingredient.new(0,[2,3],"Glühwürmchen Sekret"), Ingredient.new(1,[4,-1],"Blut"), Ingredient.new(2,[0,0],"Mondblume"), 
-Ingredient.new(3,[0,-3],"Rückenflosse einer Rotfeder"), Ingredient.new(4,[0,-2],"Fledermausflügel"), Ingredient.new(5,[2,-2],"Giftdrüse einer Schwarzen Wittve"), Ingredient.new(6,[-2,0],"Froschschenkel eines Grasfrosches")]
+var ingredients = [Ingredient.new(0,[2,3],"Glühwürmchen Sekret"), 
+Ingredient.new(1,[4,-1],"Blut"), 
+Ingredient.new(2,[0,0],"Mondblume"), 
+Ingredient.new(3,[0,-3],"Rückenflosse einer Rotfeder"), 
+Ingredient.new(4,[0,-2],"Fledermausflügel"), 
+Ingredient.new(5,[2,-2],"Giftdrüse einer Schwarzen Witwe"), 
+Ingredient.new(6,[-2,0],"Froschschenkel eines Grasfrosches")]
 
 var moonPhase = 0
 
