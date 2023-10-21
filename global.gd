0ac14296cff4ac1537d77e810b5d5feccef18e7b
Gunihelm <lennart@zurmuehls.de>
2023-10-21 10:15:51 +0200

    global classes added

---
+++ global.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class Potion:
+	var index = 0
+	var succeeded = false
+	var recepy = [0,0]
+	var color
+	var potionName = ""
+
+class Ingredient:
+	var index = 0
+	var position = [0,0]
+	var ingredientName = ""
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
