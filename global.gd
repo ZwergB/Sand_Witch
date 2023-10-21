a1f70689fe10d22cfc52e7de917a4a74fe37a88b
Gunihelm <lennart@zurmuehls.de>
2023-10-21 18:34:44 +0200

    added ingrediants

--- global.gd
+++ global.gd
@@ -2,7 +2,7 @@ extends Node2D
 class Potion:
 	var index = 0
 	var succeeded = false
-	var recepy = [0,0]
+	var recipe = [0,0]
 	var color
 	var potionName = ""
 
@@ -10,13 +10,21 @@ class Ingredient:
 	var index = 0
 	var position = [0,0]
 	var ingredientName = ""
+	func _init(_index,_position,_ingredientName):
+		index = _index
+		position = _position
+		ingredientName = _ingredientName
+var inventory = []
 
-var ingredients = [Ingredient.new(), Ingredient.new(), Ingredient.new(), 
-Ingredient.new(), Ingredient.new()]
+
+var ingredients = [Ingredient.new(0,[2,3],"Glühwürmchen Sekret"), Ingredient.new(1,[4,-1],"Blut"), Ingredient.new(2,[0,0],"Mondblume"), 
+Ingredient.new(3,[0,-3],"Rückenflosse einer Rotfeder"), Ingredient.new(4,[0,-2],"Fledermausflügel"), Ingredient.new(5,[2,-2],"Giftdrüse einer Schwarzen Wittve"), Ingredient.new(6,[-2,0],"Froschschenkel eines Grasfrosches")]
+
+var moonPhase = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
