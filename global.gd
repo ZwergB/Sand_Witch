967417476d35a1a2afd9d9d28e5478d562bad4c8
ZwergB <ben@zwergb.de>
2023-10-22 16:18:21 +0200

    Added title screen

--- global.gd
+++ global.gd
@@ -256,13 +256,24 @@ var collection = [
 func _ready():
 	pass
 
+func _input(InputEvent):
+	
+	if InputEvent is InputEventMouseButton or InputEvent is InputEventKey:		
+		if Input.is_action_pressed("ui_cancel"):
+				get_tree().quit()
+				
+		if(get_node_or_null("title") != null):
+			remove_child($title)
+		else:
+			if Input.is_action_just_pressed("ui_accept"):
+				changeScene()
+		
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_cancel"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("ui_accept"):
-		changeScene()
+	pass
 
 func changeScene():
 	if day:
